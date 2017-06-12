fc7b3e86519ea54d6f572b4a48a16c847be9639c
gahag <gabriel.s.b@live.com>
2017-06-12 00:57:53 -0300

    Updated test.py to used correct path.

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
-from os import remove
+import os
 
 import tptester
 
+
+tests_dir = os.path.dirname(os.path.realpath(__file__))
+
+out_file = tests_dir + '/output.txt'
+
 tptester.tp_test(
-  'bin/tp2',
+  tests_dir + '/../bin/tp2',
   
   indexes = [ '30', '50k', '2500k', '2500k2' ],
-  input_file = lambda ix: 'tests/data/in' + ix + '.txt',
-  answer_file = lambda ix: 'tests/data/out' + ix + '.txt',
+  input_file = lambda ix: tests_dir + '/data/in' + ix + '.txt',
+  answer_file = lambda ix: tests_dir + '/data/out' + ix + '.txt',
   
-  args = lambda ix: [ 'tests/data/in' + ix + '.txt', 'tests/output.txt' ],
+  args = lambda ix: [ tests_dir + '/data/in' + ix + '.txt', out_file ],
   stdin = lambda ix: None,
-  output = lambda ix, stdout: open('tests/output.txt', 'rb')
+  output = lambda ix, stdout: open(out_file, 'rb')
 )
 
-remove('tests/output.txt')
+os.remove(out_file)
